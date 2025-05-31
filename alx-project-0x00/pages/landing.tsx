19a64b89111d56e8e99c7a2f1c653f0c080cd8ef
Reindolf590 <150713702+Reindolf590@users.noreply.github.com>
2025-05-31 02:10:34 +0000

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import Card from "@/components/Card"
 import Button from "@/components/Button"
 
@@ -6,7 +7,6 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
 
-      <Card  />
       <Card  />
       <Card  />
 
