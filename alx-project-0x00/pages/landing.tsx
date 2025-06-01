8d5b23cfc6059373b628c90ccbe2f4813add483a
Reindolf590 <150713702+Reindolf590@users.noreply.github.com>
2025-06-01 19:12:39 +0000

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,34 +1,35 @@
-import React from "react"
-import Card from "@/components/Card"
-import Button from "@/components/Button"
+import Card from "@/components/Card";
+import Button from "@/components/Button";
 
-const Landing: React.FC =  () => {
+const Landing: React.FC = () => {
   return (
     <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card  />
-      <Card  />
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+
+      {/* Card component usage */}
+      <Card />
+      <Card />
+      <Card />
+
       <div className="space-x-4">
         <Button title="Small - sm" styles="text-sm rounded-sm" />
         <Button title="Small - md" styles="text-sm rounded-md" />
         <Button title="Small - full" styles="text-sm rounded-full" />
       </div>
 
-      {/* Medium Buttons */}
       <div className="space-x-4">
         <Button title="Medium - sm" styles="text-base rounded-sm" />
         <Button title="Medium - md" styles="text-base rounded-md" />
         <Button title="Medium - full" styles="text-base rounded-full" />
       </div>
 
-      {/* Large Buttons */}
       <div className="space-x-4">
         <Button title="Large - sm" styles="text-lg rounded-lg" />
         <Button title="Large - md" styles="text-lg rounded-md" />
         <Button title="Large - full" styles="text-lg rounded-full" />
       </div>
-
     </div>
-  )
-}
-export default Landing
+  );
+};
+
+export default Landing;
